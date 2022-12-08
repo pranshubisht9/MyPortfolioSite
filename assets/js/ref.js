0f0def8bd26680b2d22728c7087b70c94fa74ff7
Pranshu Singh Bisht <106018070+pranshubisht9@users.noreply.github.com>
2022-12-08 22:35:45 +0530

    adding files

---
+++ assets/js/ref.js
@@ -0,0 +1,31 @@
+let KettoGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/ketto.org.git',
+        '_blank'
+      );
+}
+
+let InventoryGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/Inventory_Admin_User',
+        '_blank'
+      );
+}
+
+let CodeCovGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/codeCov.io',
+        '_blank'
+      );
+}
+
+let StarWarGithub = () => {
+    window.open(
+        'https://github.com/pranshubisht9/Star_Wars',
+        '_blank'
+      );
+}
+
+GitHubCalendar(".calendar", "pranshubisht9");
+  // or enable responsive functionality
+  GitHubCalendar(".calendar", "pranshubisht9", { responsive: true });
